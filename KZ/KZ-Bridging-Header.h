1f49e4535346786ee0df41ed34f0771deb0e0dc7
Norman <ncroan@gmail.com>
2015-05-30 12:33:28 +0800

    Added JSTileMap, bridging header, and SKTUtils

---
+++ KZ/KZ-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  KZ-Bridging-Header.h
+//  KZ
+//
+//  Created by Norman Croan on 5/30/15.
+//  Copyright (c) 2015 Norman Croan. All rights reserved.
+//
+
+#ifndef KZ_KZ_Bridging_Header_h
+#define KZ_KZ_Bridging_Header_h
+
+#import "JSTileMap.h"
+
+#endif
